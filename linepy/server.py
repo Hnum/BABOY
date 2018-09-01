73928ad031d7927d300445c384defe067dc9c36e
PH-13 <31429243+PHIEZUKE@users.noreply.github.com>
2018-09-01 19:41:53 +0700

    Update server.py

--- linepy/server.py
+++ linepy/server.py
@@ -31,7 +31,7 @@ class LineServer(object):
     PHONE_TYPE  = ApplicationType.IOS
     PHONE_NAME  = 'IOS\t7.14.0\tiPhone OS\t10.12.0'
     CARRIER     = '51089, 1-0'
-    SYSTEM_NAME = 'LineBot'
+    SYSTEM_NAME = 'PH-13'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
 
